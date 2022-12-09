3e73d6a9b81206d62a3aba7f98981df573c82d31
Aidan Villegas <120068538+aidanvillegas@users.noreply.github.com>
2022-12-09 13:48:09 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,8 @@
-//Noah Molla
-//Mr. Kahn Period 4
-//Ready, Set... Code! S2E4 (Simple OOP Database) - [Due: Sun 11/15 @ 11:59PM]
+
+
+//Simple OOP Database - [Due: Sun 11/15 @ 11:59PM]
 //November 30th, 2020
-//Sources: https://www.youtube.com/watch?v=0KdG9DZgi6I 
-//https://www.youtube.com/watch?v=CWYv7xlKydw&ab_channel=AlexLee
-//Collaborated with Aidan Villegas
+
 
 import javax.swing.JFrame;
 class main{
